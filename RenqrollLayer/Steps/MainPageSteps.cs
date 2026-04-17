1eae3119beb6ed7f94efda7ccebc948b6b0360ab
Zhan Kariahin <Zhan.Kariahin@gmail.com>
2026-04-17 14:51:11 +0300

    Added Main Page + search test.

--- RenqrollLayer/Steps/MainPageSteps.cs
+++ RenqrollLayer/Steps/MainPageSteps.cs
@@ -1,60 +1,44 @@
-﻿//using NUnit.Framework;
-//using OpenQA.Selenium;
-//using Reqnroll;
-//using ReqnrollLayer.Enum;
-//using ReqnrollLayer.Pages;
-//using ReqnrollLayer.WebDriver;
+﻿using NUnit.Framework;
+using Reqnroll;
+using ReqnrollLayer.Pages;
 
-//namespace ReqnrollLayer.Steps
-//{
-//    [Binding]
-//    internal class MainPageSteps
-//    {
-//        private readonly IWebDriver driver;
-//        private readonly MainPage mainPage;
+namespace ReqnrollLayer.Steps
+{
+    [Binding]
+    internal class MainPageSteps
+    {
+        private readonly DriverContext context;
+        private readonly MainPage mainPage;
 
-//        public MainPageSteps()
-//        {
-//            ChromeDriverFactory driverFactory = new ChromeDriverFactory();
-//            this.driver =  driverFactory.CreateDriver(WebBrowserMode.UXUI);
-//            this.mainPage = new MainPage(this.driver);
-//        }
+        public MainPageSteps(DriverContext context)
+        {
+            this.context = context;
+            this.mainPage = new MainPage(this.context.Driver!);
+        }
 
-//        [Given(@"I navigate to the EPAM website")]
-//        public void GivenINavigateToEPAMWebsite()
-//        {
-//            //indexPage.Open();
-//        }
+        [When(@"I click on the Search icon element")]
+        public void WhenIClickOnTheSearchIconElement()
+        {
+            this.mainPage.ClickSearchIcon();
+        }
 
-//        [When(@"I click on the Search icon element")]
-//        public void WhenIClickOnTheSearchIconElement()
-//        {
-//            //indexPage.ClickSearchIcon();
-//        }
+        [When(@"I enter the text '(.*)' into the search input")]
+        public void WhenIEnterTheTextIntoTheSearchInput(string searchText)
+        {
+            this.mainPage.InputDataIntoSearchInput(searchText);
+        }
 
-//        [When(@"I enter the text '(.*)' into the search input")]
-//        public void WhenIEnterTheTextIntoTheSearchInput(string searchText)
-//        {
-//            //indexPage.EnterSearchText(searchText);
-//        }
+        [When(@"I click on the Find button")]
+        public void WhenIClickOnTheFindButton()
+        {
+            this.mainPage.ClickFindButton();
+        }
 
-//        [When(@"I click on the Find button")]
-//        public void WhenIClickOnTheFindButton()
-//        {
-//            //indexPage.ClickFindButton();
-//        }
-
-//        [Then(@"The list of search results is displayed on the page")]
-//        public void ThenTheListOfSearchResultsIsDisplayedOnThePage()
-//        {
-//            //bool isDisplayed = indexPage.AreSearchResultsDisplayed();
-//            //Assert.IsTrue(isDisplayed);
-//        }
-
-//        [AfterScenario]
-//        public void AfterScenario()
-//        {
-//            this.driver.Quit();
-//        }
-//    }
-//}
+        [Then(@"The list of search results is displayed on the page")]
+        public void ThenTheListOfSearchResultsIsDisplayedOnThePage()
+        {
+            bool isDisplayed = this.mainPage.AreSearchResultsDisplayed();
+            Assert.IsTrue(isDisplayed);
+        }
+    }
+}
